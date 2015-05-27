b50a23c037413288716a02d2c8a388d4926ee194
gonzaloea <gonza2703@gmail.com>
2015-05-27 20:36:52 -0300

    Cambios clase ListadoCartas
    Se corrigió nombre de properties. Se agregó la funcionalidad del método
    "sacar_carta".

--- listado_cartas.py
+++ listado_cartas.py
@@ -4,10 +4,9 @@ class ListadoCartas(object):
     que ya se vieron, para saber cuales conviene consultar."""
     def __init__(self, personajes_inicial, armas_inicial, lugares_inicial):
         """Recibe un iterable para las cartas de personajes, armas y lugares."""
-        self.personajes_inicial=personajes_inicial
-        self.armas_inicial=armas_inicial
-        self.lugares_inicial=lugares_inicial
-        raise NotImplementedError()
+        self.personajes=personajes_inicial
+        self.armas=armas_inicial
+        self.lugares=lugares_inicial
 
     def __str__(self):
         """Convierte el listado en una cadena"""
@@ -15,4 +14,11 @@ class ListadoCartas(object):
 
     def sacar_carta(self, carta):
         """Saca una determinada carta de los listados de personajes, armas y lugares (los marca como "vistos")"""
-        raise NotImplementedError()
+        if carta in self.personajes:
+			self.personajes.remove(carta)
+		elif carta in self.armas:
+			self.armas.remove(carta)
+		elif carta in self.lugares:
+			self.lugares.remove(carta)
+		else:
+			raise ValueError()
