57e198fd52b999bf92b0be3982fcf4c49843d405
gonzaloea <gonza2703@gmail.com>
2015-05-28 23:55:15 -0300

    Arreglo de tabs y encoding
    Se cambian espacios por tabs.

--- juego.py
+++ juego.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 from interfaz_juego import InterfazJuego
 from interfaz_jugador import InterfazJugador
 from tablero import Tablero
--- pruebas_tp3.py
+++ pruebas_tp3.py
@@ -1,4 +1,3 @@
-
 # Importa el paquete para realizar pruebas automatizadas de Python.
 import unittest
 from unittest import TestCase
@@ -12,57 +11,57 @@ from tablero import Tablero
 # Esta clase esta dedicada a probar la clase de DadoEstandar.
 class TestDadoEstandar(TestCase):
 
-    # Cada metodo debe probar una funcionalidad de la clase.
-    # Este, por ejemplo, verifica que las probabilidades de cada cara son iguales.
-    def test_probabilidades_son_equiprobables(self):
-        # Dentro de un metodo realizo la prueba:
+	 # Cada metodo debe probar una funcionalidad de la clase.
+	 # Este, por ejemplo, verifica que las probabilidades de cada cara son iguales.
+	 def test_probabilidades_son_equiprobables(self):
+		# Dentro de un metodo realizo la prueba:
 
-        # Creo un dado estandar de seis caras.
-        dado = DadoEstandar(6)
+		# Creo un dado estandar de seis caras.
+		dado = DadoEstandar(6)
 
-        # Obtengo sus probabilidades.
-        # Este metodo no se usa en el juego pero se agrego para poder correr la prueba.
-        probabilidades = dado.obtener_probabilidades()
+		# Obtengo sus probabilidades.
+		# Este metodo no se usa en el juego pero se agrego para poder correr la prueba.
+		probabilidades = dado.obtener_probabilidades()
 
-        prob = probabilidades[0]
-        for i in range(1, len(probabilidades)):
-            # Se usan los metodos de unittest para hacer verificaciones.
-            self.assertEqual(prob, probabilidades[i])
+		prob = probabilidades[0]
+		for i in range(1, len(probabilidades)):
+				# Se usan los metodos de unittest para hacer verificaciones.
+				self.assertEqual(prob, probabilidades[i])
 
-    # Este otro metodo, por ejemplo, verifica que las probabilidades de ocurrencia de
-    # cada cara no se puedan modificar.
-    def test_probabilidades_no_se_deben_modificar(self):
-        dado = DadoEstandar(6)
-        probabilidades = dado.obtener_probabilidades()
+	 # Este otro metodo, por ejemplo, verifica que las probabilidades de ocurrencia de
+	 # cada cara no se puedan modificar.
+	 def test_probabilidades_no_se_deben_modificar(self):
+		dado = DadoEstandar(6)
+		probabilidades = dado.obtener_probabilidades()
 
-        # Modifico el resultado devuelto.
-        probabilidad_anterior = probabilidades[0]
-        probabilidades[0] = probabilidad_anterior + 1
+		# Modifico el resultado devuelto.
+		probabilidad_anterior = probabilidades[0]
+		probabilidades[0] = probabilidad_anterior + 1
 
-        # Verifico que no se haya modificado su probabilidad dentro del dado.
-        self.assertEqual(probabilidad_anterior, dado.obtener_probabilidades()[0])
+		# Verifico que no se haya modificado su probabilidad dentro del dado.
+		self.assertEqual(probabilidad_anterior, dado.obtener_probabilidades()[0])
 
 
 class TestTablero(TestCase):
 
-    # Prueba que la creacion usando listas de diferentes largos levante una excepcion.
-    def test_inicializacion_con_distintos_largos_invalida(self):
-        casilleros = ['1', None, '3']
-        posiciones = ['1', '2']
+	 # Prueba que la creacion usando listas de diferentes largos levante una excepcion.
+	 def test_inicializacion_con_distintos_largos_invalida(self):
+		casilleros = ['1', None, '3']
+		posiciones = ['1', '2']
 
-        # Estructura que verifica que se levante una excepcion de tipo ValueError
-        # dentro del bloque with.
-        with self.assertRaises(ValueError):
-            Tablero(casilleros, posiciones)
+		# Estructura que verifica que se levante una excepcion de tipo ValueError
+		# dentro del bloque with.
+		with self.assertRaises(ValueError):
+				Tablero(casilleros, posiciones)
 
-    # Prueba que los casilleros no se puedan modificar desde afuera.
-    def test_casilleros_no_se_deben_modificar(self):
-        raise NotImplementedError()
+	 # Prueba que los casilleros no se puedan modificar desde afuera.
+	 def test_casilleros_no_se_deben_modificar(self):
+		raise NotImplementedError()
 
 
-# Si este archivo crece mucho quizas sea necesario dividirlo...
-
+#class TestListadoCartas(TestCase):
+#	pass
 
 # Ejecuta todas las pruebas cuando se ejecute este archivo.
 if __name__ == "__main__":
-    unittest.main()
+	 unittest.main()
