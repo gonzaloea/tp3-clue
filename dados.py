845d2c38daad235d12e66aea71cb3c7f74d7bc3f
gonzaloea <gonza2703@gmail.com>
2015-05-28 22:42:32 -0300

    Probabilidad triangular de dados
    Se agrego la funcionalidad para probabilidades triangulares para los
    dados en la clase DadoTriangular.
    Adicionalmente se agrego el código de encoding para que funcione
    correctamente al ejecutar.

--- dados.py
+++ dados.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 TIPO_DADO_ESTANDAR = "Estandar (todas las caras equiprobables)"
 TIPO_DADO_CRECIENTE = "Creciente"
 TIPO_DADO_DECRECIENTE = "Decreciente"
@@ -66,7 +67,38 @@ class DadoTriangular(Dado):
     las caras cercanas al valor medio tienen mayor probabilidad, y a medida que nos alejamos
     de dicho valor la probabilidad va disminuyendo."""
     def __init__(self, caras):
-        raise NotImplementedError()
+        inicial = 1
+		final = caras
+		medio = math.ceil(float(final)/2)
+		prob_caras=[]	
+		medio_par = -1
+		
+		#Para que cuando es par sean iguales los del medio.
+		if caras % 2 == 0:
+			medio_par = medio+1
+			
+		#Casos especiales en donde el calculo no funciona
+		if caras == 1:
+			prob_caras = [1]
+		if caras == 2:
+			prob_caras = [0.5,0.5]
+		else:
+		#Si no son los casos especiales hago el cálculo.
+			for valor in range(inicial,final+1):
+				prob=None
+				if valor >= inicial and valor < medio:
+					prob = float(2*(valor-inicial))/((final-inicial)*(medio-inicial))
+				elif valor == medio:
+					prob = 2/float(final-inicial)
+					prob = 2/float(final-inicial)
+				elif  valor == medio_par:
+					prob = 2/float(final-inicial)
+					medio = medio_par
+				elif valor > medio and valor <= final:
+					prob = float(2*(final-valor))/((final-inicial)*(final-medio))
+				if prob is not None:
+					prob_caras.append(prob)
+		Dado.__init__(prob_caras)
 
 
 GENERADORES = [DadoEstandar, DadoCreciente, DadoDecreciente, DadoTriangular]
